59f57012416117dab6e712a9cc4b36b2eadc2e4c
quix <quixoticsycophant@gmail.com>
2011-03-01 17:10:01 -0500

    add tests for define_method :f, &b

--- test/define_method_test.rb
+++ test/define_method_test.rb
@@ -1,7 +1,7 @@
 require_relative 'main'
 
 class DefineMethodTest < RegularTest
-  DEFINE = lambda do
+  WITH_BLOCKS = lambda do
     class A
       {
         :f => :+,
@@ -26,8 +26,8 @@ class DefineMethodTest < RegularTest
     end
   end
 
-  def test_define_method
-    DEFINE.call
+  def test_with_block
+    WITH_BLOCKS.call
 
     assert_equal binop_define_method_with_var(:name, :+),
                  A.instance_method(:f).to_ast
@@ -37,5 +37,31 @@ class DefineMethodTest < RegularTest
 
     assert_equal binop_define_method_with_var(:name, :-),
                  A.instance_method(:h).to_ast
+  end
+
+  WITH_PROCS = lambda do
+    class B
+      op = lambda { |x, y| x / y }
+      
+      no_arg = proc { "B#f" }
+
+      define_method :g, &no_arg ; define_method :f, &op
+    end
+  end
+
+  def test_via_block
+    WITH_PROCS.call
+
+    assert_equal binop_block(:lambda, :/),
+                 B.instance_method(:f).to_ast
+
+    assert_equal binop_block(:lambda, :/),
+                 B.new.method(:f).to_ast
+
+    assert_equal no_arg_block(:proc, "B#f"),
+                 B.instance_method(:g).to_ast
+
+    assert_equal no_arg_block(:proc, "B#f"),
+                 B.new.method(:g).to_ast
   end
 end
