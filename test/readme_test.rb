36dcf4c7ccb692361d12a71cd69d4f18e3732572
Matijs van Zuijlen <matijs@matijs.net>
2014-08-11 11:57:19 +0200

    Remove redundant test parser check
    The tests always run with the same parser, so we don't have to check it.

--- test/readme_test.rb
+++ test/readme_test.rb
@@ -1,9 +1,7 @@
 require_relative 'main'
 require_relative '../devel/levitate'
 
-if LiveAST.parser::Test.respond_to?(:unified_sexp?) and
-    LiveAST.parser::Test.unified_sexp? and
-    RUBY_ENGINE != "jruby" then  # jruby takes about a minute
+if RUBY_ENGINE != "jruby" # jruby takes about a minute
   sections = [
     "Synopsis",
     "+to_ruby+",
